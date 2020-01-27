a4a6b7ebedf54952d9ba3ab9b90d17d5e3246f94
Jama Mohamed <jama3137@gmail.com>
2020-01-27 10:18:15 +0300

    Got response from dialogflow

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,8 +1,6 @@
 from app.utils.validate import isQueryValid, isQueryEmpty
 from app.utils.return_messages import success, error
-import requests
 from os import environ
-import json
 
 
 def detect_intent(req):
@@ -10,4 +8,45 @@ def detect_intent(req):
         return error(404, "Please make sure to have a valid param in the url, that is the 'query' param")
     elif not isQueryEmpty(req):
         return error(422, "Looks like you didn't pass anything")
-    return success(200, "This is ok")
+
+    res = getReponseFromDialogflow(req.args.get("query"))
+    
+
+
+    return success(200, res)
+
+
+def getReponseFromDialogflow(query):
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(
+        environ.get("DIALOGFLOW_PROJECT_ID"), "123456789")
+    print('Session path: {}\n'.format(session))
+
+    text_input = dialogflow.types.TextInput(
+        text=query, language_code="en-US")
+
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
+
+    print('=' * 20)
+    print('Query text: {}'.format(response.query_result.query_text))
+    print('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    print('Fulfillment text: {}\n'.format(
+        response.query_result.fulfillment_text))
+
+    return {
+      "intent": response.query_result.intent.display_name,
+      "fullfillment_text": response.query_result.fulfillment_text,
+      "params": {
+          "number": str(response.query_result.parameters.fields["number"]),
+          "reddit-category": str(response.query_result.parameters.fields["reddit-category"]),
+          "reddit-type": str(response.query_result.parameters.fields["reddit-type"]),
+          "subreddit": str(response.query_result.parameters.fields["subreddit"])
+      }
+    }
--- app/views/views.py
+++ app/views/views.py
@@ -1,15 +1,15 @@
-from flask import Blueprint, jsonify, request, render_template
+from flask import Blueprint, jsonify, request, render_template, make_response
 from app.repository.repository import detect_intent
 
 views = Blueprint("views", __name__)
 
 
-@views.route("", methods=["GET"])
+@views.route("")
 def renderReactPage():
     return "React Page Here", 200
 
 
-@views.route("chatbot", methods=["POST"])
+@views.route("chatbot")
 # @cross_origin()
 def chatbot():
     response = detect_intent(request)
