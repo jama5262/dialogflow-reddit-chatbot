6732300cff872f45de070a19beee86a54724d2f8
Jama Mohamed <jama3137@gmail.com>
2020-01-27 10:56:16 +0300

    Cleaned dialogflow response

--- app/repository/repository.py
+++ app/repository/repository.py
@@ -1,6 +1,7 @@
 from app.utils.validate import isQueryValid, isQueryEmpty
 from app.utils.return_messages import success, error
 from os import environ
+import re
 
 
 def detect_intent(req):
@@ -10,8 +11,8 @@ def detect_intent(req):
         return error(422, "Looks like you didn't pass anything")
 
     res = getReponseFromDialogflow(req.args.get("query"))
-    
 
+    
 
     return success(200, res)
 
@@ -24,29 +25,24 @@ def getReponseFromDialogflow(query):
         environ.get("DIALOGFLOW_PROJECT_ID"), "123456789")
     print('Session path: {}\n'.format(session))
 
-    text_input = dialogflow.types.TextInput(
-        text=query, language_code="en-US")
-
+    text_input = dialogflow.types.TextInput(text=query, language_code="en-US")
     query_input = dialogflow.types.QueryInput(text=text_input)
-
     response = session_client.detect_intent(
         session=session, query_input=query_input)
 
-    print('=' * 20)
-    print('Query text: {}'.format(response.query_result.query_text))
-    print('Detected intent: {} (confidence: {})\n'.format(
-        response.query_result.intent.display_name,
-        response.query_result.intent_detection_confidence))
-    print('Fulfillment text: {}\n'.format(
-        response.query_result.fulfillment_text))
+    numberReg = re.search(
+        "\\d+", str(response.query_result.parameters.fields["number"]))
+
+    def stringReg(value):
+        return re.search("(?<=string_value: \\\").*(?=\\\"\\n)", str(response.query_result.parameters.fields[value]))
 
     return {
-      "intent": response.query_result.intent.display_name,
-      "fullfillment_text": response.query_result.fulfillment_text,
-      "params": {
-          "number": str(response.query_result.parameters.fields["number"]),
-          "reddit-category": str(response.query_result.parameters.fields["reddit-category"]),
-          "reddit-type": str(response.query_result.parameters.fields["reddit-type"]),
-          "subreddit": str(response.query_result.parameters.fields["subreddit"])
-      }
+        "intent": response.query_result.intent.display_name,
+        "fullfillment_text": response.query_result.fulfillment_text,
+        "params": {
+            "number": numberReg.group(0) if numberReg is not None else 0,
+            "reddit-category": stringReg("reddit-category").group(0) if stringReg("reddit-category") is not None else "",
+            "reddit-type": stringReg("reddit-type").group(0) if stringReg("reddit-type") is not None else "",
+            "subreddit": stringReg("subreddit").group(0) if stringReg("subreddit") is not None else ""
+        }
     }
