74e879d748a73f318511e8cd295d6a39e749cecc
Jama Mohamed <jama3137@gmail.com>
2020-02-02 12:55:45 +0300

    Added the react build files

---
+++ app/templates/precache-manifest.5b7e1ad8c83f0b0a0ad8a627f563d520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69bf091b1d50d4c0f0782a8c4c1aeaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3686a5188b9c1948b157",
+    "url": "/static/css/main.53c42184.chunk.css"
+  },
+  {
+    "revision": "f3117673c9fdab530fc8",
+    "url": "/static/js/2.2dfb27f4.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.2dfb27f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3686a5188b9c1948b157",
+    "url": "/static/js/main.70b0759c.chunk.js"
+  },
+  {
+    "revision": "ecdb8f32aff71d42c698",
+    "url": "/static/js/runtime-main.fd795ecf.js"
+  }
+]);
